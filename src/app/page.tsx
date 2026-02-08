f1807a31f83c2d16b3d549e5b451a093c4ea27e2
Nithin <nithin.shsr@gmail.com>
2026-02-08 22:11:06 +0530

    Create How It Works section content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import WhatToAsk from "../components/Landing Page/WhatToAsk"
 import PricingSection from "../components/Landing Page/PricingSection"
 import CTA from "../components/Landing Page/CTA"
 import Footer from "../components/Landing Page/Footer"
-
+import HowItWorks from "../components/Landing Page/HowItWorks"
 
 export default function Home() {
     return (
        <div className="min-h-screen bg-background">
            <Header/>
            <Hero/>
+           <HowItWorks/>
            <WhatToAsk/>
            <PricingSection/>
            <CTA/>
